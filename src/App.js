42526c77b5aefb97727d3f626c476d91c342eca8
Martin <martinstojanovic96@gmail.com>
2023-05-04 16:34:27 +0200

    add donation component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import './App.css';
 import Main from './Components/Main/Main';
 import Footer from './Components/Footer/Footer';
+import Donation from "./Components/Donation/Donation";
 
 function App() {
 
   return (
     <div className="app">
       <Main />
+      <Donation />
       <Footer />
     </div>
   );
---
+++ src/Components/Donation/Donation.jsx
@@ -0,0 +1,14 @@
+import './donation.css';
+
+export default function Donation() {
+    return (
+        <div className="donation">
+            <div>
+                If you find this app useful consider donating crypto or GU cards to my wallet
+            </div>
+            <div>
+                Address:  <span className="address" > 0x5edA277A26c44AB616bD1c334Ab89674C32916c7</span>
+            </div>
+        </div>
+    );
+}
