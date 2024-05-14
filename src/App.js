d37c3b324c46ff95b7568ddad1fff689d933f8d9
Jiggzawyr <martinstojanovic96@gmail.com>
2024-05-14 21:41:05 +0200

    remove donations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import './App.css';
-import Main from './Components/Main/Main';
-import Footer from './Components/Footer/Footer';
-import Donation from "./Components/Donation/Donation";
+import "./App.css";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-
   return (
     <div className="app">
       <Main />
-      <Donation />
       <Footer />
     </div>
   );
